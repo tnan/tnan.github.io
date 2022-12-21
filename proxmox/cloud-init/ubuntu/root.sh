96f22ac7980e93f6a3bdf2277f26dff3623384a3
tnan <mr.tnan@live.com>
2022-12-20 23:31:19 -0800

    Update root.sh

--- proxmox/cloud-init/ubuntu/root.sh
+++ proxmox/cloud-init/ubuntu/root.sh
@@ -1,2 +1,4 @@
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+history -c
+poweroff
